1b0c90554833b7eb81d033ea9c4a32d61dfaaf70
mirzaRakha28 <mirzarakhadwipradana28@gmail.com>
2019-11-10 01:10:53 +0700

    membuat folderdasboard admin di control dan view

---
+++ Ecommerce/application/controllers/admin/dasboard_admin.php
@@ -0,0 +1,14 @@
+<?php
+
+    class Dasboard_admin extends CI_Controller{
+
+        public function index (){
+            
+            $this->load->view('admin/dasboard');
+
+
+        }
+
+    }
+
+?>
