48fea948533e1214eee6337e07215fafe33b38ad
mirzaRakha28 <mirzarakhadwipradana28@gmail.com>
2019-11-10 01:28:46 +0700

    menambah file template_admin footer , header dan sidebar

--- Ecommerce/application/controllers/admin/dasboard_admin.php
+++ Ecommerce/application/controllers/admin/dasboard_admin.php
@@ -3,8 +3,11 @@
     class Dasboard_admin extends CI_Controller{
 
         public function index (){
-            
+            $this->load->view('templates/header');
+            $this->load->view('templates/sidebar');
             $this->load->view('admin/dasboard');
+            $this->load->view('templates/footer');
+            
 
 
         }
