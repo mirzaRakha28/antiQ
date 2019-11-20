e2db4f601186651029b44edb5abbc0ea84b42b4d
Farhan Akbar <56985728+farhanakbr@users.noreply.github.com>
2019-11-20 17:27:53 +0700

    Add files via upload
    menambahkan view pada fitur_person untuk bagian login dan regist

--- Ecommerce/application/views/fitur_person/person_0.php
+++ Ecommerce/application/views/fitur_person/person_0.php
@@ -1,40 +1,32 @@
 
-<!-- Nav tabs, ini tombol tab di atas -->
-<ul class="nav nav-tabs">
-<!-- Untuk Semua Tab.. pastikan a href=”#nama_id” sama dengan nama id di “Tap Pane” dibawah-->
-  <li class="active"><a href="#new" data-toggle="tab">New Customer</a></li>
-  <li><a href="#old" data-toggle="tab">Old Customer</a></li>
-</ul>
-<!-- Tab panes, ini content dari tab di atas -->
-<div class="text-center">
-    <div class="tab-content">
-        <div class="tab-pane" id="new">
-        <div class="modal-body">
-                <p> <b>Login Account</p>
-                <div class="form-group">
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email Address    ">
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <label  class="btn btn-secondary active" style="background-color: Black; ">
-                <input type="radio" name="options" id="option1" autocomplete="off" checked> Login
-                </label>
+<div class="login ">
+    <div class="text-center ">
+        <div class="costumer">    
+            <div class="text-center costumer_log "style= "float: left   ">
+                <a href="<?php echo site_url('fitur_person/person_1') ?>">New Costumer</a>
+                <hr>
+            </div>
+            <div class="text-center costumer_log" style= "float: right">
+                <a href="">Old Custumer</a>
+                <hr>
             </div>
         </div>
-        <div class="tab-pane" id="old">
-            <div class="modal-body">
-                <p> <b>Login Account</p>
-                <div class="form-group">
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email Address    ">
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <label  class="btn btn-secondary active" style="background-color: Black; ">
-                <input type="radio" name="options" id="option1" autocomplete="off" checked> Login
-                </label>
-            </div>
+        <form>
+    <div class="text-center" >
+        <h2 class="text-judul">
+            <b>Login Account</b>
+        </h1>
+        <div class="form-group input_data">
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
         </div>
+        <div class="form-group input_data">
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
         </div>
+        <div class="form-group login_submit">
+            <button type="submit" class="btn btn-primary login_submit">Login</button>
+        </div>
+        </form>
+        </div>
+    </div>
+    </div>
 </div>
---
+++ Ecommerce/application/views/fitur_person/person_1.php
@@ -0,0 +1,41 @@
+
+<div class="login ">
+    <div class="text-center ">
+        <div class="costumer">    
+            <div class="text-center costumer_log "style= "float: left   ">
+                <a href="">New Costumer</a>
+                <hr>
+            </div>
+            <div class="text-center costumer_log" style= "float: right">
+                <a href="<?php echo site_url('fitur_person/person_0') ?>">Old Custumer</a>
+                <hr>
+            </div>
+        </div>
+        <form>
+    <div class="text-center" >
+        <h2 class="text-judul">
+            <b>Create Account</b>
+        </h1>
+        <div class="form-group input_data">
+            <input type="name" class="form-control" id="exampleInputName1" placeholder="Name">
+        </div>
+        <div class="form-group input_data">
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email Address">
+        </div>
+        <div class="form-group input_data">
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="form-group input_data">
+            <input type="confirmpassword" class="form-control" id="exampleInputConfirmPassowrd1" placeholder="Confirm Password">
+        </div>
+        <div class="form-group input_data">
+            <input type="gender" class="form-control" id="exampleInputGender1" placeholder="Gender">
+        </div>
+        <div class="form-group login_submit">
+            <button type="submit" class="btn btn-primary login_submit">Register</button>
+        </div>
+        </form>
+        </div>
+    </div>
+    </div>
+</div>
