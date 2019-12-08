4f3dcd84cf6026aef7892f371c9589b64c9f8e58
mirzaRakha28 <54165216+mirzaRakha28@users.noreply.github.com>
2019-12-08 10:43:17 +0700

    menambahkan add 2 keranjang

--- Ecommerce/application/controllers/fitur_cart/Cart.php
+++ Ecommerce/application/controllers/fitur_cart/Cart.php
@@ -49,6 +49,18 @@
           $this->cart->insert($data);
           redirect('fitur_whatsnew/whatsnew');
         }
+		public function add2_keranjang ($id){
+          $barang = $this->model_barang->find($id);
+          $data = array(
+              'id'      => $barang->id_barang,
+              'qty'     => 1,
+              'price'   => $barang->harga,
+              'name'    => $barang->nama_barang,
+              'gambar' => $barang->gambar
+          );
+        $this->cart->insert($data);
+        redirect('fitur_search/search');
+      }
         public function hapus_data(){
           $this->cart->destroy();
           redirect('fitur_cart/cart/keranjang');
