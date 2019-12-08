105d61f2ec2dd3538e1b91902cab90c493441053
mirzaRakha28 <54165216+mirzaRakha28@users.noreply.github.com>
2019-12-08 10:45:32 +0700

    menambhakan segalanya

--- Ecommerce/application/controllers/fitur_search/Search.php
+++ Ecommerce/application/controllers/fitur_search/Search.php
@@ -27,33 +27,25 @@ class Search extends CI_Controller {
 			foreach($data->result() as $row)
 			{
 				$output .= '
-
-						<div class="col-lg-4 col-md-3 mb-4 float-left">
-                        <div class="card card-ecommerce">
-                        <div class="view overlay">
-						<img src="'.base_url("uploads/.$row->gambar.").'" style="width: 419px; height: 169.98px" alt="">
-							<a>
-								<div class="mask rgba-white-slight"></div>
-							</a>
-						</div>
-                            <div class="card-body">
-                                <h5 class="card-title mb-1"><strong><a href="" class="dark-grey-text">'.$row->nama_barang.'</a></strong></h5>
-                                <span class="badge badge-info mb-2">'.$row->kategori.'</span>
-                                <br>
-                                <span class="text-center"><a>'.$row->keterangan.'</a></span>
-                                <div class="card-footer pb-0">
-                                    <div class="row mb-0">
-                                        <span class="float-left"><strong>'.$row->harga.'</strong></span>
-                                    </div>
-                                    <div class="text-center">
-                                        <a style="cursor: pointer;" class=" btn btn-primary text-white tambah_barang" > tambah </a>
-                                        <a style="cursor: pointer;" class="btn btn-primary text-white" id="">Detail</a>
-                                    </div>
-
-									</div>
-									</div>
-								</div>
+					<div class="span m-4"> 
+						<div class="card text-center " style="width: 16rem; float: left;">
+							<img src="'.base_url("/uploads/$row->gambar").'" class="card-img-top" style="width: 250px;
+							height: 250px; margin: auto; padding: 10px">
+							<div class="produk">
+								<p class="font-weight-bold text-uppercase">
+									'.$row->nama_barang.'
+								</p>
+								<small class="font-italic">Category: '.$row->kategori.'</small>
+								<p>
+									Price: Rp'.number_format($row->harga,0,',','.').'
+								</p>
+							<div style="padding: 10px"> 
+									'.anchor('fitur_cart/cart/add2_keranjang/'.$row->id_barang,'<div class="btn btn-primary btn-block" style="margin:auto; background-color: #730006; outline-style: no-outline;" id="tambah_barang">Add to Cart</div>').'
+									<a style="cursor: pointer; margin:6px 0px ;  " class="btn btn-outline-dark btn-block" id="">Detail</a>
+								</div>  
 							</div>
+						</div>
+					</div>
 				';
 			}
 		}
